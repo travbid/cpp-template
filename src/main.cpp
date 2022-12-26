828159f411452248dda39d48df19f4d3f80d9298
Travers Biddle <traversbiddle@gmail.com>
2022-12-26 15:31:42 +1300

    Print result of main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "add.hpp"
 
+#include <iostream>
+
 int main(int argc, char **argv) {
     const float result = add(static_cast<float>(argc), static_cast<float>(argv[0][0]));
+    std::cout << "result: " << result << "\n";
     return static_cast<int>(result);
 }
